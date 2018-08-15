09a239285fda415afb9fa5c07c32c74d47640689
github username <github email address>
2018-08-15 17:41:28 -0400

    display data

--- src/components/ShowLoans.js
+++ src/components/ShowLoans.js
@@ -4,27 +4,61 @@ import '../App.css';
 
 class ShowLoans extends Component {
 
+  state = {
+    clicked: true
+  }
+
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    })
+    console.log(this.state.clicked)
+  }
+
   test2 = () => {
-    if(!this.props.loans){
-        console.log('it aint there')
-    } else {
-      console.log(this.props.loans[0])
-      console.log(this.props.loans)
+    if(this.props.loans){
 
       return this.props.loans.map(loanInfo => (
         <div className="LoanContainer">
+          <label>75% LTV Proceeds</label>
+          <br/>
           <div>{loanInfo['75% LTV Proceeds']}</div>
+          <label># Payments</label>
+          <br/>
           <div>{loanInfo['# Payments']}</div>
+          <label>Agency</label>
+          <br/>
           <div>{loanInfo.Agency}</div>
+          <label>Annual Debt Service</label>
+          <br/>
           <div>{loanInfo['Annual Debt Service']}</div>
+          <label>Debt Constant</label>
+          <br/>
           <div>{loanInfo['Debt Constant']}</div>
+          <label>Interest Rate</label>
+          <br/>
           <div>{loanInfo['Interest Rate']}</div>
+          <label>NOI</label>
+          <br/>
           <div>{loanInfo.NOI}</div>
+          <label>Payoff</label>
+          <br/>
           <div>{loanInfo.Payoff}</div>
+          <label>Proceeds</label>
+          <br/>
           <div>{loanInfo.Proceeds}</div>
+          <label>Treasury</label>
+          <br/>
           <div>{loanInfo.Treasury}</div>
+          <label>Type</label>
+          <br/>
           <div>{loanInfo.Type}</div>
+          <label>Value</label>
+          <br/>
           <div>{loanInfo.Value}</div>
+          <label>Years</label>
+          <br/>
           <div>{loanInfo.Years}</div>
         </div>
       ))
